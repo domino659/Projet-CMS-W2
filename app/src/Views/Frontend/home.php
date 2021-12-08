6422b2a96518a0cfa428599da6b2274f5fe9e0dd
Sammy Ferrier <sammyferrier@MacBook-Pro-de-Sammy.local>
2021-12-08 19:18:33 +0100

    view article

--- app/src/Views/Frontend/home.php
+++ app/src/Views/Frontend/home.php
@@ -1,13 +1,17 @@
 <h1>Un titre de home page</h1>
 
 <?php
+
 /**
  * @var $user \App\Entity\Author
  * @var $posts \App\Entity\Post[]
  */
 
-
-
-echo $test;
-
-?>
+foreach ($posts as $article) :
+    ?>
+    <div>
+        <h2><?= $article->getTitle(); ?></h2>
+        <p><?= substr($article->getContent(), 0, 200); ?></p>
+        <a href="/article/<?= $article->getId(); ?>">Lire plus</a>
+    </div>
+<?php endforeach; ?>
