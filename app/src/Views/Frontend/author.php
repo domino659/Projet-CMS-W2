156c4c5ba004886626da89eb2d2805077529291c
Martin SION <martin@sionfamily.com>
2021-12-10 04:28:00 +0100

    added delete post

--- app/src/Views/Frontend/author.php
+++ app/src/Views/Frontend/author.php
@@ -1,4 +1,5 @@
 <h1>Je suis la page auteur</h1>
+
 <?php
 if (\App\Fram\Utils\Flash::hasFlash('alert')): ?>
     <div class="alert alert-danger" role="alert">
@@ -25,9 +26,8 @@ foreach ($authors as $user) :
             <label for="isAdmin">Is Admin</label>
         </div>
         <form action="deleteUser" method="post">
-            <input type="hidden" name="current_user_id" value="<?= $_SESSION['user_token']['id'] ?>">
-            <input type="hidden" name="target_user_id" value="<?= $user->getId(); ?>">
-            <input name="delete" type="submit" value="Delete" onclick="return confirm('Are you sure?')">
+                <input type="hidden" name="target_user_id" value="<?= $user->getId(); ?>">
+                <input name="delete" type="submit" value="Delete" onclick="return confirm('Are you sure?')">
         </form>
     </div>
 <?php endforeach;?>
